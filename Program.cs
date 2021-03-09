856987786454226ba657bf951296801ba5d916f8
Dwe29 <wahyu5161.w5@gmail.com>
2021-03-09 12:05:30 +0800

    ganti tanda seru

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace belajarGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }

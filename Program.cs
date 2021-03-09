d95d128844d0b9c8915f5186e8e7ad82c1a158e0
Dwe29 <wahyu5161.w5@gmail.com>
2021-03-09 12:01:55 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace belajarGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
